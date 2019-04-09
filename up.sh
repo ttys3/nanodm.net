9fcc719d2750f9f6a5dcbb451afeefde3a34a90f
荒野無燈 <i@80x86.io>
2019-04-10 01:12:56 +0800

    update up.sh

--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 hugo
-rsync --rsh='ssh -p29684' -ravzzP --checksum --delete --out-format="%t | %f%L | %l | %b"  ./public/ root@files.80x86.io:/home/http/html/nanodm.net/
+rsync --rsh='ssh -p29684' -ravzzP --checksum --delete public/ root@files.80x86.io:/home/http/html/nanodm.net/
 
